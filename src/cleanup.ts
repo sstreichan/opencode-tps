5fefbabbf00e488c6ba657a9c550b097b9f70e44
sebastian <sebastian.streichan@senseaition.com>
2026-06-12 08:39:26 +0200

    Revert "feat: modularize TPS plugin into src/ modules with tests"
    This reverts commit bd39ecf1689aff93e85a90d0f94523c4d4439cd4.

--- src/cleanup.ts
+++
@@ -1,9 +0,0 @@
-import type { PluginState } from "./types";
-
-export function disposeAll(state: PluginState) {
-  state.messageStats.clear();
-  state.sessionMeta.clear();
-  state.lastKnownTps.clear();
-  state.streamSamples.clear();
-  state.completedSessions.clear();
-}
